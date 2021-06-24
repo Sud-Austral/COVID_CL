8e9ab5eadfc035b4fd74e85c19d20144b2138e6f
Ian Meza <ianmeza29@gmail.com>
2021-06-24 16:53:13 -0600

    Commit

---
+++ Servidor.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+#************************************Bucle general********************************************************
+def UpdateDatabase():
+    print("Comenzo...")
+    df = pd.read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto80/vacunacion_comuna_1eraDosis.csv")
+    df.to_excel("Vacuna/Vacunacion2.xlsx")
+    return
+
+#************************************Actualizar Datos de la organizacion*******************************************
+
+if __name__ == '__main__':
+    print('Iniciado proceso...')
+    UpdateDatabase()
+    print('Proceso finalizado.')
