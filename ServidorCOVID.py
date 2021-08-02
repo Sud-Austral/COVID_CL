2aeecc94a707f14630ceda5f0391e1a12518667e
lmonsalve22 <lmonsalve22@gmail.com>
2021-08-01 20:53:19 -0400

    Update ServidorCOVID.py

--- ServidorCOVID.py
+++ ServidorCOVID.py
@@ -6,8 +6,9 @@ def ConvertirFecha(fecha):
 def DescargarPasoAPaso():
     d = pd.read_csv("https://github.com/MinCiencia/Datos-COVID19/raw/master/input/Paso_a_paso/paso_a_paso.csv")
     dfinal = d.melt(id_vars=d.columns[:5], var_name='Fecha', value_name='Etapa')
-    dfinal["Fecha"] = dfinal["Fecha"].apply(ConvertirFecha) 
-    dfinal.to_excel("COVID/Paso a Paso.xlsx")
+    #dfinal["Fecha"] = dfinal["Fecha"].apply(ConvertirFecha) 
+    dfinal["Fecha"] = pd.to_datetime(dfinal["Fecha"]).dt.date
+    dfinal.to_excel("COVID/Paso a Paso.xlsx", index=False)
     return
 
 def UpdateDatabase():
